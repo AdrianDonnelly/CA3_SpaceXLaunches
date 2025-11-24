a9d462a65a5c9c452303f321dd2638409c869b3f
AdrianDonnelly <X00194620@mytudublin.ie>
2025-11-24 14:11:41 +0000

    MudBlazor Framework added

--- X00194620_Ca3/Program.cs
+++ X00194620_Ca3/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using X00194620_Ca3;
+using MudBlazor.Services;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
+builder.Services.AddMudServices();
 builder.Services.AddScoped(sp => new HttpClient{ BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
